5a9f7f4efb140b4adf2cdff3578d58eec96df264
Pahul Brar <brarstar14@macbookpro.ucalgary.ca>
2025-04-07 16:08:59 -0600

    Adjusted Header to make it invisible

--- src/app/(tabs)/favourites/_layout.tsx
+++ src/app/(tabs)/favourites/_layout.tsx
@@ -1,10 +1,12 @@
 import { Stack } from "expo-router";
 import { View } from "react-native";
 import { defaultStyles } from "@/styles";
+import { StackScreenWithSearchBar } from "@/constants/layout";
 const FavouritesScreenLayout = () => {
     return <View style={defaultStyles.container}>
         <Stack>
             <Stack.Screen name = "index" options={{ 
+                ...StackScreenWithSearchBar,
                 headerTitle: "Favourites",
             }} />
         </Stack>
--- src/app/(tabs)/playlists/_layout.tsx
+++ src/app/(tabs)/playlists/_layout.tsx
@@ -1,10 +1,12 @@
 import { Stack } from "expo-router";
 import { View } from "react-native";
 import { defaultStyles } from "@/styles";
+import { StackScreenWithSearchBar } from "@/constants/layout";
 const PlaylistsScreenLayout = () => {
     return <View style={defaultStyles.container}>
         <Stack>
             <Stack.Screen name = "index" options={{ 
+                ...StackScreenWithSearchBar,
                 headerTitle: "Songs",
             }} />
         </Stack>
